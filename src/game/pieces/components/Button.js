f1cc5bf37adb18473a1707bc441321abc7b23e02
amiani <amianijohns@gmail.com>
2018-10-04 12:17:49 -0400

    added Button

---
+++ src/game/pieces/components/Button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: inline-block;
+  flex-shrink: 0;
+  height: 20px;
+  border: solid black 2px;
+  user-select: none;
+  margin: 1px;
+  padding: 1px 3px 0px 3px;
+  font-size: 12px;
+  text-align: center;
+
+  &:hover {
+    background-color: green;
+  }
+`;
+
+export default props => (
+  <Container onClick={props.onClick}>
+    {props.children}
+  </Container>
+);
