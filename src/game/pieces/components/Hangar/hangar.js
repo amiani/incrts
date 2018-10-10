6c5ba8e3cf8ae72fb95846bf88aa9d10055d2cbb
amiani <amianijohns@gmail.com>
2018-10-10 14:58:31 -0400

    removed semicolons

--- src/game/pieces/components/Hangar/hangar.js
+++ src/game/pieces/components/Hangar/hangar.js
@@ -1,10 +1,10 @@
-import uuidv4 from 'uuid/v4';
+import uuidv4 from 'uuid/v4'
 
 export default function ProtoHangar(ownerId, isSource) {
-  this.id = uuidv4();
-  this.ownerId = ownerId;
-  this.capacity = 10;
-  this.isSource = isSource;
-  this.demand = { tanks: 0 };
-  this.units = { tanks: [], };
+  this.id = uuidv4()
+  this.ownerId = ownerId
+  this.capacity = 10
+  this.isSource = isSource
+  this.demand = { tanks: 0 }
+  this.units = { tanks: [], }
 }
