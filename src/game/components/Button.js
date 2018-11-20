7df8dd84d76a3872c12cd9b79d25aee6c8141946
ratang <amianijohns@gmail.com>
2018-11-20 08:36:38 -0500

    changed button theme

--- src/game/components/Button.js
+++ src/game/components/Button.js
@@ -5,17 +5,17 @@ const Box = styled.div`
   display: inline;
   flex-shrink: 0;
   height: 20px;
-  border: solid 2px #4B2C97;
+  border: solid 2px #ee855e; 
   border-radius: 3px;
   margin: 1px;
   padding: 1px 3px 1px 3px;
   font-size: 12px;
   text-align: center;
-  background-color: #EBE4FE;
+  //background-color: #EBE4FE;
 
   :hover {
-    color: #ffffff;
-    background-color: #4B2C97;
+    color: #002836;
+    background-color: #ee855e; 
   }
 `
 
